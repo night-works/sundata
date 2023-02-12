afb03c081340d7b48ac72e1681ab6753a1f7b1f3
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2023-02-12 23:44:35 +0000

    get lighting period for the position of the sun

--- sundata/sundata.py
+++ sundata/sundata.py
@@ -3,6 +3,7 @@ from astropy.coordinates import AltAz,get_sun
 from astropy.time import Time
 import astropy.units as u
 from datetime import datetime
+from enum import Enum
 
 from dataclasses import dataclass
 
@@ -12,6 +13,16 @@ class Position:
     longitude:float
 
 
+
+class LightPeriod(Enum):
+
+    NIGHT = 0
+    ASTRO = 1
+    NAUTICAL = 2
+    CIVIL = 3
+    DAY = 4
+
+
 def get_sun_altitude(position:Position, when:datetime) -> float:
     earth_location = coord.EarthLocation(lon=position.longitude * u.deg,
                           lat=position.latitude * u.deg)
@@ -19,4 +30,17 @@ def get_sun_altitude(position:Position, when:datetime) -> float:
     altazframe = AltAz(obstime=when, location=earth_location)                                               
     sunaltaz = get_sun(when).transform_to(altazframe)  
     return sunaltaz.alt.max().value
+    
+
+def lighting_is(sun_altitude:float) -> LightPeriod:
+    if sun_altitude >= 0:
+        return LightPeriod.DAY
+    if sun_altitude < -18:
+        return LightPeriod.NIGHT
+    if sun_altitude < 0 and sun_altitude >= -6:
+        return LightPeriod.CIVIL
+    if sun_altitude < -6 and sun_altitude >= -12:
+        return LightPeriod.NAUTICAL
+    if sun_altitude < -12 and sun_altitude >= -18:
+        return LightPeriod.ASTRO
     
--- tests/test_sun_altitude.py
+++ tests/test_sun_altitude.py
@@ -1,4 +1,4 @@
-from sundata.sundata import Position, get_sun_altitude
+from sundata.sundata import Position, get_sun_altitude, lighting_is
 from datetime import datetime
 
 
@@ -18,3 +18,9 @@ def test_get_altitude_20():
     when = datetime(2023,2,12,19,16)
     sun = get_sun_altitude(pos,when)
     assert  int(sun) == -20
+    
+def test_get_light_period_day():
+    pos = Position(51.772938,0.102310)  
+    when = datetime(2023,2,12,19,16)
+    sun = get_sun_altitude(pos,when)
+    print(lighting_is(sun))
