a5f040384d8e121eb989e5bcd109380b1582bf9f
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2023-02-12 23:45:36 +0000

    formatting of files

--- sundata/sundata.py
+++ sundata/sundata.py
@@ -1,5 +1,5 @@
 import astropy.coordinates as coord
-from astropy.coordinates import AltAz,get_sun
+from astropy.coordinates import AltAz, get_sun
 from astropy.time import Time
 import astropy.units as u
 from datetime import datetime
@@ -7,15 +7,14 @@ from enum import Enum
 
 from dataclasses import dataclass
 
+
 @dataclass
 class Position:
-    latitude:float
-    longitude:float
-
+    latitude: float
+    longitude: float
 
 
 class LightPeriod(Enum):
-
     NIGHT = 0
     ASTRO = 1
     NAUTICAL = 2
@@ -23,16 +22,17 @@ class LightPeriod(Enum):
     DAY = 4
 
 
-def get_sun_altitude(position:Position, when:datetime) -> float:
-    earth_location = coord.EarthLocation(lon=position.longitude * u.deg,
-                          lat=position.latitude * u.deg)
-    when = Time(when, format='datetime', scale='utc')                                  
-    altazframe = AltAz(obstime=when, location=earth_location)                                               
-    sunaltaz = get_sun(when).transform_to(altazframe)  
+def get_sun_altitude(position: Position, when: datetime) -> float:
+    earth_location = coord.EarthLocation(
+        lon=position.longitude * u.deg, lat=position.latitude * u.deg
+    )
+    when = Time(when, format="datetime", scale="utc")
+    altazframe = AltAz(obstime=when, location=earth_location)
+    sunaltaz = get_sun(when).transform_to(altazframe)
     return sunaltaz.alt.max().value
-    
 
-def lighting_is(sun_altitude:float) -> LightPeriod:
+
+def lighting_is(sun_altitude: float) -> LightPeriod:
     if sun_altitude >= 0:
         return LightPeriod.DAY
     if sun_altitude < -18:
@@ -43,4 +43,3 @@ def lighting_is(sun_altitude:float) -> LightPeriod:
         return LightPeriod.NAUTICAL
     if sun_altitude < -12 and sun_altitude >= -18:
         return LightPeriod.ASTRO
-    
